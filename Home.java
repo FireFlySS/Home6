3ff3dfbe7d4a2079f26ca823b96ab266c0baef10
FireFlySS <mishafrend2017@yandex.ru>
2018-09-01 11:42:55 +0300

    Старунов

---
+++ Home.java
@@ -0,0 +1,27 @@
+public class Home {
+public  Home(){
+
+}
+public boolean proverka(int arr[]){
+
+    boolean res = true;
+    int count1 = 0;
+    int count4 = 0;
+    int i = 0;
+    while (i < arr.length && res) {
+        if(arr[i] == 1) {
+            res = true;
+            count1++;
+        } else {
+            if(arr[i] == 4) {
+                res = true;
+                count4++;
+            } else {
+                res = false;
+            }
+        }
+        i++;
+    }
+    return res && count1 > 0 && count4 > 0;
+}
+}
